f30a62d8a4a0457a126c7604ddace0fbbdcd3ba5
Zhenhua Li <zhenhuali@google.com>
2025-02-24 15:04:09 -0800

    chore: put the tgc revival and tgc in the same version (#3506)

--- pkg/cai2hcl/converters/resource_converters.go
+++ pkg/cai2hcl/converters/resource_converters.go
@@ -15,7 +15,6 @@ package converters
 
 import (
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/cai2hcl/converters/services/compute"
-	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/cai2hcl/converters/services/container"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/cai2hcl/converters/services/resourcemanager"
 	"github.com/GoogleCloudPlatform/terraform-google-conversion/v7/pkg/cai2hcl/models"
 
@@ -27,7 +26,6 @@ var provider *schema.Provider = tpg_provider.Provider()
 
 // ConverterMap is a collection of converters instances, indexed by cai asset type.
 var ConverterMap = map[string]models.Converter{
-	resourcemanager.ProjectAssetType:    resourcemanager.NewProjectConverter(provider),
-	compute.ComputeInstanceAssetType:    compute.NewComputeInstanceConverter(provider),
-	container.ContainerClusterAssetType: container.NewContainerClusterConverter(provider),
+	resourcemanager.ProjectAssetType: resourcemanager.NewProjectConverter(provider),
+	compute.ComputeInstanceAssetType: compute.NewComputeInstanceConverter(provider),
 }
